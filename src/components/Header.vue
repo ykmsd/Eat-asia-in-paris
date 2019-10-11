e10a84969bb5348bb7f5bd715c3997a65d2219c9
ykmsd <yuka1110m@gmail.com>
2019-10-11 22:48:29 +0200

    Create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="black-70">
+    <h1 class="f1 lh-title">{{ $props.title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style scoped>
+header {
+  font-family: var(--title-font);
+  text-align: center;
+  border-top: 10px solid var(--main-color-dark);
+}
+</style>
