ff5840f11fa3e7ceb7476caa1176f6bf29cb70b3
ykmsd <yuka1110m@gmail.com>
2019-10-13 00:35:20 +0200

    Add contact

---
+++ src/views/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tc">
+    <p>
+      I hate emails please DM me on
+      <a
+        href="https://instagram.com/eatasiainparis"
+        target="_blank"
+      >Instagram</a>!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
